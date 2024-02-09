6efef44b7ce193246a2b6dbb3c7f285f7ef40091
Juan Manuel Valencia Ramírez <57605265+Juan-Valencia-Ramirez@users.noreply.github.com>
2024-02-08 20:52:57 -0500

    Update .DS_Store

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -6,32 +6,36 @@ public class BirdScript : MonoBehaviour
 {
     public Rigidbody2D myrigidbody;
     public float flatStrength;
+    public bool birdIsAlive = true;
+    public LogicScript logicGrowing;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        logicGrowing = GameObject.FindGameObjectWithTag("LogicGrowing").GetComponent<LogicScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W) == true )
+        if (Input.GetKeyDown(KeyCode.W))
 
         {
             myrigidbody.velocity = Vector2.up * flatStrength;
         }
 
-        if (Input.GetKeyDown(KeyCode.D) == true)
+        if (Input.GetKeyDown(KeyCode.D))
 
         {
             myrigidbody.velocity = Vector2.right * flatStrength;
         }
 
-        if (Input.GetKeyDown(KeyCode.A) == true)
+        if (Input.GetKeyDown(KeyCode.A))
 
         {
             myrigidbody.velocity = Vector2.left * flatStrength;
         }
     }
+
+    
 }
